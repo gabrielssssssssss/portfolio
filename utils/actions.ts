b000cfa13bebb88910ff8655f38b91ca86f40a18
gabrielssssssssss <974gabriel.contact@gmail.com>
2025-09-16 20:31:18 +0200

    Music.tsx connect with prisma db

--- utils/actions.ts
+++ utils/actions.ts
@@ -3,6 +3,11 @@ import { NextResponse } from "next/server";
 import { prisma } from "@/utils/db";
 
 export async function GET() {
-  const users = await prisma.stack.findMany()
-  return users;
+  const data = await prisma.stack.findMany()
+  return data;
+}
+
+export async function GET_MUSIC() {
+  const data = await prisma.music.findMany()
+  return data;
 }
